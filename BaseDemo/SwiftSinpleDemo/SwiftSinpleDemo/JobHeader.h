dcf885134e7bfa5b7218cfb641967d2483a3624c
whjool <whengjie@126.com>
2016-10-23 22:49:44 +0800

    check
    check

--- BaseDemo/SwiftSinpleDemo/SwiftSinpleDemo/JobHeader.h
+++
@@ -1,9 +0,0 @@
-//
-//  JobHeader.h
-//  SwiftSinpleDemo
-//
-//  Created by HUN on 16/10/23.
-//  Copyright © 2016年 com.hundred.valueTransfer. All rights reserved.
-//
-
-#define UserDefaultName @"user"
