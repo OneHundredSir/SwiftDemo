4b640b65d306120f962104fd11af2139a9d1e9ce
whjool <whengjie@126.com>
2016-11-01 20:29:40 +0800

    add bookDetail
    show

--- BaseDemo/SwiftSinpleDemo/SwiftSinpleDemo/Single.swift
+++ BaseDemo/SwiftSinpleDemo/SwiftSinpleDemo/Single.swift
@@ -1,14 +1,12 @@
 //
 //  Single.swift
 //  SwiftSinpleDemo
-//
-//  Created by HUN on 16/10/22.
-//  Copyright © 2016年 com.hundred.valueTransfer. All rights reserved.
-//
+//  作者:香蕉大大  简书地址：http://www.jianshu.com/users/a3ae6d7c68b6/latest_articles
+//  微博:http://weibo.com/u/6013257513?is_all=1
+//  QQ技术讨论交流群 : 365204530
+//  微信个人技术公众号：大大家的IOS说（一起用碎片时间学习最新最有用的IOS资料）
 
 import UIKit
-
-
 
 //MARK: 单例:方法2
 let single = Single()
@@ -17,11 +15,7 @@ let single = Single()
 class Single: NSObject {
     //MARK - : 单例:方法1
     static let shareSingleOne = Single()
-   
-    
-    
-    
-    
+
     //-MARK: 单例:方法2
     class var sharedInstance2 : Single {
             return single
